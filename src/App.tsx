de605a170cb97d33974b03ac14b8e7d8d03127a4
Anner Henriquez <annerh3@gmail.com>
2026-05-15 14:36:49 -0600

    feat: update permission page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router"
 import PhoneLoginPage from "./pages/PhoneLoginPage"
+import { PermissionFormPage } from "./pages/PermissionFormPage"
 
 export function App() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-background p-4">
       <Routes>
         <Route path="/" element={<PhoneLoginPage />} />
+        <Route path="/acceso/:tokenId" element={<PermissionFormPage />} />
       </Routes>
     </main>
   )
