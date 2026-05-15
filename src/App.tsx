ad9a76ff339859e75e6ad36239860c12e53a4d32
MichaelGald <michaelcom292@gmail.com>
2026-05-15 14:32:57 -0600

    feat: Add AdminMainPage.tsx and supa

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router"
 import PhoneLoginPage from "./pages/PhoneLoginPage"
+import { AdminMainPage } from "./pages/AdminMainPage"
 
 export function App() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-background p-4">
       <Routes>
         <Route path="/" element={<PhoneLoginPage />} />
+        <Route path="/admin" element={<AdminMainPage />} />
       </Routes>
     </main>
   )
